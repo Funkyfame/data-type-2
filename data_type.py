5a34d5b0e52a18a086e6bef755ea8fcfbaee7341
Funkyfame <flourishingfaithy@gmail.com>
2022-10-05 20:32:16 +0100

    data types in python

---
+++ data_type.py
@@ -0,0 +1,39 @@
+# dictionary
+# key and value
+# {}
+# {'lagos': 'ikeja',
+#   'car': 'tesla',
+#   'date':'birtdate'
+# }
+
+my_stuff    =   {'laptop':'HP',     "Nationality":"Nigerian",   'OS':"Linux", 'OS':"Linux"}
+# print(len(my_stuff))
+# print(type(my_stuff))
+
+# my_stuff['Nationality'] =   'United State'
+# my_stuff.update({'laptop': 'GFORCE'})
+# my_stuff['Sneakers'] =   'Nike'
+print(my_stuff)
+
+# indentation
+
+
+# arguments
+# paramenters
+# topic
+# from python 3.6 downward
+# from python 3.7 upward 
+
+def my_funct():
+    
+    pass 
+
+my_funct()
+
+class my_class:
+    pass 
+
+
+# method 
+# function
+# class
